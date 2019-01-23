4ebca353dee61ab58a6af2fd941ee319c97fdfb8
Glauber Magalhães <glauber.tecinfo@gmail.com>
2019-01-23 16:02:23 -0300

    Formatted the code to become human readable.

--- problem_1.py
+++ problem_1.py
@@ -1,3 +1,14 @@
 import requests
-r = requests.get('https://data.lacity.org/resource/ngkp-kqkn.json?$query=SELECT COUNT(business_name), business_name GROUP BY business_name ORDER BY COUNT_business_name DESC LIMIT 1')
-print(r.json())
+
+apiURL = "https://data.lacity.org/resource/ngkp-kqkn.json"
+query = "SELECT COUNT(business_name), business_name GROUP BY business_name ORDER BY COUNT_business_name DESC LIMIT 1"
+endpoint = '''{0}?$query={1}'''.format(apiURL, query)
+request = requests.get(endpoint)
+result = request.json()
+
+output = '''The business with the most locations in Los Angeles is {0}, with {1} locations'''.format(
+    result[0][u'business_name'],
+    result[0][u'COUNT_business_name'],
+)
+
+print(output)
--- problem_2.py
+++ problem_2.py
@@ -1,3 +1,18 @@
 import requests
-r = requests.get('https://data.lacity.org/resource/ngkp-kqkn.json?$query=SELECT business_name,location_start_date ORDER BY location_start_date ASC LIMIT 1')
-print(r.json())
+import datetime
+
+apiURL = "https://data.lacity.org/resource/ngkp-kqkn.json"
+query = "SELECT business_name, location_start_date ORDER BY location_start_date ASC LIMIT 1"
+endpoint = '''{0}?$query={1}'''.format(apiURL, query)
+request = requests.get(endpoint)
+result = request.json()
+
+businessName = result[0][u'business_name']
+foundedDate = datetime.datetime.strptime(result[0][u'location_start_date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m-%d')
+
+output = '''The oldest active business in Los Angeles is {0}, founded in {1}'''.format(
+    businessName,
+    foundedDate
+)
+
+print(output)
